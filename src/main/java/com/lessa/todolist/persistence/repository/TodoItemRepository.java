2b7c65bf1aba521998d9a46da517789a0c464ca6
Davi Lessa <daviarcangelo@gmail.com>
2024-03-19 21:58:54 +0100

    TodoItemRepository#updatePastDueItemsStatus query implemented

--- src/main/java/com/lessa/todolist/persistence/repository/TodoItemRepository.java
+++ src/main/java/com/lessa/todolist/persistence/repository/TodoItemRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -14,9 +16,8 @@ public interface TodoItemRepository extends JpaRepository<TodoItemEntity, UUID> 
 
     Page<TodoItemEntity> findAllByStatus(Status status, Pageable pageable);
 
-    @Modifying
-    //TODO @Query("")
-    int updatePastDueItemsStatus(LocalDateTime actualDateTime);
-
+    @Modifying(clearAutomatically = true)
+    @Query("update TodoItemEntity item set item.status = 'PAST_DUE' where item.status = 'NOT_DONE' and item.dueDate >= :currentDateTime")
+    int updatePastDueItemsStatus(@Param("currentDateTime") LocalDateTime currentDateTime);
 
 }
