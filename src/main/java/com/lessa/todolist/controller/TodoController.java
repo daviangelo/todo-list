bdd7242c69ceaf1e2b88e608a0df348d98de4c8f
Davi Lessa <daviarcangelo@gmail.com>
2024-03-20 16:03:24 +0100

    TodoController put /todos/{id} endpoint added

--- src/main/java/com/lessa/todolist/controller/TodoController.java
+++ src/main/java/com/lessa/todolist/controller/TodoController.java
@@ -1,15 +1,17 @@
 package com.lessa.todolist.controller;
 
+import com.lessa.todolist.dto.ChangeDescriptionDto;
 import com.lessa.todolist.dto.CreateItemDto;
 import com.lessa.todolist.dto.TodoItemDto;
 import com.lessa.todolist.service.TodoService;
 import com.lessa.todolist.service.exception.ConflictException;
+import com.lessa.todolist.service.exception.NotFoundException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +22,13 @@ public class TodoController {
     @PostMapping("/todos")
     public ResponseEntity<TodoItemDto> addItem(@RequestBody @Valid CreateItemDto createItemDto) throws ConflictException {
         var domain = service.add(createItemDto.toDomain());
+        return ResponseEntity.ok(TodoItemDto.fromDomain(domain));
+    }
+
+    @PutMapping("/todos/{id}")
+    public ResponseEntity<TodoItemDto> updateDescription(@PathVariable UUID id,
+                                                         @RequestBody @Valid ChangeDescriptionDto changeDescriptionDto) throws ConflictException, NotFoundException {
+        var domain = service.updateDescription(id, changeDescriptionDto.getDescription());
         return ResponseEntity.ok(TodoItemDto.fromDomain(domain));
     }
 
